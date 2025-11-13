5cdaeb844f3904c2d346e1cbb15edf37af922736
neverleaveualong <wmr06244@naver.com>
2025-11-14 04:01:13 +0900

    feat: 요약분석 API추가

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { signup, login, logout, refresh } from "../controllers/authController";
-import { requireAuth } from "../middlewares/requireAuth";
 const router = Router();
 
 router.post("/signup", signup);
