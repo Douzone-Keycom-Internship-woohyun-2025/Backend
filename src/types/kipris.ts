5c00d195d36ebc85f87f21f416c12a98dd153109
neverleaveualong <wmr06244@naver.com>
2025-11-15 10:07:38 +0900

    feat: 특허목록 API 구현

---
+++ src/types/kipris.ts
@@ -0,0 +1,24 @@
+export interface PatentItemRaw {
+  applicantName?: string;
+  applicationDate?: string;
+  applicationNumber?: string;
+  inventionTitle?: string;
+  ipcNumber?: string;
+  openDate?: string;
+  openNumber?: string;
+  publicationDate?: string | null;
+  publicationNumber?: string | null;
+  registerDate?: string | null;
+  registerNumber?: string | null;
+  registerStatus?: string;
+  astrtCont?: string;
+  bigDrawing?: string;
+  drawing?: string;
+}
+
+export interface PatentListResult {
+  total: number;
+  page: number;
+  totalPages: number;
+  patents: PatentItemRaw[];
+}
