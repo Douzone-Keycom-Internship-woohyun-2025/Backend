a60ccf2d908ba6692d545807d6c4e7b0ea170faa
neverleaveualong <wmr06244@naver.com>
2025-11-16 10:10:22 +0900

    feat: 즐겨찾기 특허 타입추가

---
+++ src/types/favorite.ts
@@ -0,0 +1,31 @@
+export interface FavoriteRow {
+  patent_tblkey: number;
+  user_tblkey: number;
+  invention_title: string;
+  applicant_name: string;
+  abstract: string | null;
+  application_date: string;
+  application_number: string;
+  publication_date: string | null;
+  publication_numner: string | null;
+  register_date: string | null;
+  register_number: string | null;
+  register_status: string | null;
+  drawing_url: string | null;
+  adddate: Date;
+  open_number: string | null;
+}
+
+export interface FavoritePayload {
+  inventionTitle: string;
+  applicantName: string;
+  abstract?: string;
+  applicationDate: string;
+  applicationNumber: string;
+  publicationDate?: string;
+  publicationNumber?: string;
+  registerDate?: string;
+  registerNumber?: string | null;
+  registerStatus?: string;
+  drawingUrl?: string;
+}
